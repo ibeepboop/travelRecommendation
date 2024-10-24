7408b7ca3ed52c466c91c9870872bdf72c0e4c10
ibeepboop <michellemeyer660@gmail.com>
2024-10-23 23:12:33 -0400

    addded images and basic fetch() js

---
+++ js/travel_recommendation.js
@@ -0,0 +1,15 @@
+// get travel recommendation data from json form
+async function getData() {
+  const url = "/travelRecommendation/data/travel_recommendation_api.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
